ca04470889ab0914b4d2d69c2b4c2f9c33138e93
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-29 20:39:07 +0530

    ADD:SingletCombination

--- flip2.sh
+++ flip2.sh
@@ -1,10 +1,44 @@
-echo "Welcomee to FlipCoinSimulator"
+echo "Welcome to FlipCoinSimulator"
 
-flip=$((RANDOM%2))
+function flipCoin(){
 
-if(( flip == 1 ))
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi
+declare -A combination
+num=$1
+key=""
+
+for (( i=0;i<50;i++ ))
+do
+
+	for ((j=0;j<$num;j++))
+	do
+		flip=$((RANDOM%2))
+
+	if (( flip == 1 ))
+	then
+		key=$key"HEADS"
+	else
+		key=$key"TAILS"
+	fi
+	done
+
+	combination[$key]=$(( ${combination[$key]} + 1 ))
+	key=""
+done
+
+	percentage ${!combination[@]}
+}
+
+function percentage(){
+
+result=${@}
+echo
+
+for i in $result
+do
+	j=${combination[$i]}
+	echo "$i	 $(( ($j * 100 ) / 30 ))"
+
+done | sort -k2 -nr
+}
+
+flipCoin 1
